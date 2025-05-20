dcfd2962f8a7600095e1c20dd5e018a6f2eb2f03
stefantencate <stefan.ten.cate@me.com>
2025-05-20 16:42:31 +0200

    Initisl setup SamenBuurten (wagtail , django and allauth)

---
+++ samenbuurten/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-3o)4_kmhm2uke#3(p*g#vc9hqw@kafs-czzm^w-#ctj8rip54t"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
